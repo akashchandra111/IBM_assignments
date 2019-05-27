bbdb88eb9116bf6f5015a67b2f4216956c0f5916
AkashChandra <AkashChandra@DESKTOP-JLJEFRL.in.ibm.com>
2019-05-27 15:34:21 +0530

    added all the valid scenarios for testing in test file

--- payment_demo_app/src/com/ibm/unittest/WalletUnitTestCases.java
+++ payment_demo_app/src/com/ibm/unittest/WalletUnitTestCases.java
@@ -8,34 +8,73 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import com.ibm.payment.bean.UserAccount;
 import com.ibm.payment.service.ServiceWallet;
+import com.ibm.payment.service.ValidateUserAccount;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class WalletUnitTestCases {
 
-	private static UserAccount account;
-	//final private static ServiceWallet wallet;
+	private static UserAccount account, anotherAccount;
+	final private static ServiceWallet wallet;
 	
 	static	{
 		account = new UserAccount();
-		//wallet = new ServiceWallet();
+		anotherAccount = new UserAccount();
+		wallet = new ServiceWallet();
 	}
 	
 	//UserAccount Tests
 	@Test
-	void userAccountTest()	{
+	void userAccountTests()	{
 		
-		account.setAcctHolderName("Akash");
-		account.setAcctNo("200321343");
+		account.setAcctHolderName("Akash Chandra");
+		account.setAcctNo("12395481");
 		account.setAcctHolderContact("8979583828");
 		account.setAcctHolderAddr("bangalore");
 		
+		anotherAccount.setAcctHolderName("Akash Chandra");
+		anotherAccount.setAcctNo("12394345");
+		anotherAccount.setAcctHolderContact("8979583828");
+		anotherAccount.setAcctHolderAddr("bangalore");
+		
 		assertAll(
-			() -> assertEquals("Akash", account.getAcctHolderName(), "fails!"),
-			() -> assertEquals("200321343", account.getAcctNo(), "fails!"),
+			() -> assertEquals("Akash Chandra", account.getAcctHolderName(), "fails!"),
+			() -> assertEquals("12395481", account.getAcctNo(), "fails!"),
 			() -> assertEquals("8979583828", account.getAcctHolderContact(), "fails!"),
 			() -> assertEquals("bangalore", account.getAcctHolderAddr(), "fails!")
 		);
 	}
-
+	
+	//Validations Tests
+	@Test
+	void userValidationTests()	{
+		assertAll(
+			() -> assertEquals(true, ValidateUserAccount.validateUser("Akash Chandra"), "name validation failed!"),
+			() -> assertEquals(false, ValidateUserAccount.validateUser("Akash-Chandra"), "name validation failed!"),
+			
+			() -> assertEquals(true, ValidateUserAccount.validatePhone("8979583828"), "phone validation failed!"),
+			() -> assertEquals(false, ValidateUserAccount.validatePhone("8979583828A"), "phone validation failed!"),
+			
+			() -> assertEquals(true, ValidateUserAccount.validateMoney("12000"), "money validation failed!"),
+			() -> assertEquals(false, ValidateUserAccount.validateMoney("1200000000000"), "money validation failed!"),
+			
+			() -> assertEquals(true, ValidateUserAccount.validateAddress("35C bangalore"), "address validation failed!"),
+			() -> assertEquals(false, ValidateUserAccount.validateAddress("35C; bangalore"), "address validation failed!"),
+			
+			()-> assertEquals(true, ValidateUserAccount.validateAcctNo("321439847"), "account no. validation failed!"),
+			()-> assertEquals(false, ValidateUserAccount.validateAcctNo("39213fja"), "account no. validation failed!")
+		);
+	}
+	
+	//Wallet Service Tests
+	@Test
+	void serviceWalletTests()	{
+		assertAll(
+			() -> assertEquals(true, wallet.checkUser(account),"checkUser failed!"),
+			() -> assertEquals(false, wallet.checkUser(anotherAccount), "checkUser failed!"),
+			
+			() -> assertEquals("16090", wallet.getBalance(account), "getBalance failed")
+			//,() -> assertThrows(WalletProblem.class, () -> wallet.getBalance(anotherAccount), "getBalance failed")
+		);
+	}
 }
 
