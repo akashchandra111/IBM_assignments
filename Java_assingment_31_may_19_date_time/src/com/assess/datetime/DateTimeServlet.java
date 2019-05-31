78af65530f3e2ee2a35188528787b771256b7fef
Akash Chandra <akasch23@in.ibm.com>
2019-05-31 15:54:16 +0530

    done uploading assignment 1 of 31-05-2019

---
+++ Java_assingment_31_may_19_date_time/src/com/assess/datetime/DateTimeServlet.java
@@ -0,0 +1,25 @@
+package com.assess.datetime;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/datetime")
+public class DateTimeServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Date date = new Date();
+		response.getWriter().println("Date: " + date);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
